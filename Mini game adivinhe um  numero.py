26e452e8cac822a6340a4bc5054250eb236ebb08
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2022-08-12 15:26:55 -0300

    Add files via upload

---
+++ Mini game adivinhe um  numero.py
@@ -0,0 +1,8 @@
+palpite =  9
+numero  =   9
+print("==============Bemvindo  ao jogo  adivinhe ou morra===================")
+while palpite !=  numero:
+      palpite = int(input("Digite um  numero:"))
+
+print("Parabéns voce  acertou ")
+print(bool(3>8))
